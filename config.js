3a7b8c6371dc352888f892a15227a74c5bc58109
ArthemisCOF <Sorawit.Ily@hotmail.com>
2024-04-09 17:26:32 +0700

    fix config

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ const path = require('path');
 
 dotenv.config({path: path.join(__dirname, `./.env.${process.env.NODE_ENV}`)});
 
-const {PORT, HOST} = process.env;
+const {PORT, HOST, SQL_USER, SQL_PASSWORD, SQL_DATABASE} = process.env;
+
+const sqlEncrypt = process.env.ENCRYPT
 
 assert(PORT, 'PORT is require');
 assert(HOST, 'HOST is require');
@@ -12,5 +14,14 @@ assert(HOST, 'HOST is require');
 
 module.exports = {
     port: PORT,
-    host: HOST
+    host: HOST,
+    sql: {
+        database: SQL_DATABASE,
+        user: SQL_USER,
+        password: SQL_PASSWORD,
+        option: {
+            encrypt: sqlEncrypt,
+            enableArithAbort: true
+        }
+    }
 }
